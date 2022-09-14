d015f864d275dc7f6427088eac9b2a286c3059ce
Yuri <yurifdev@gmail.com>
2022-09-14 17:12:22 -0300

    Chaging the then hell to async and await

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var consultaCEP = fetch("http://viacep.com.br/ws/01001000/json/") //fetch é um método assincrono com um parâmetro obrigatório, a URL
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) { //Caso o formato do CEP seja válido, porém inexistente, o retorno de 'erro' será verdadeiro. https://viacep.com.br/
-      throw Error("Erro na requisição, verifique o CEP");
-    } else {
-      console.log("O endereço encontrado é: " , r);
-    }
-  })
-  .catch((erro) => console.log(erro))  //Catch quando a 'promise' é rejeitada
-  .finally(mensagem => (console.log('Processamento concluído'))); //Independentemente da resposta, o "finally" irá acontecer.
-console.log(consultaCEP);
+// var consultaCEP = fetch("http://viacep.com.br/ws/01001000/json/") //fetch é um método assincrono com um parâmetro obrigatório, a URL
+//   .then((resposta) => resposta.json())
+//   .then((r) => {
+//     if (r.erro) {
+//       //Caso o formato do CEP seja válido, porém inexistente, o retorno de 'erro' será verdadeiro. https://viacep.com.br/
+//       throw Error("Erro na requisição, verifique o CEP");
+//     } else {
+//       console.log("O endereço encontrado é: ", r);
+//     }
+//   })
+//   .catch((erro) => console.log(erro)) //Catch quando a 'promise' é rejeitada
+//   .finally((mensagem) => console.log("Processamento concluído")); //Independentemente da resposta, o "finally" irá acontecer.
+// console.log(consultaCEP);
+
+async function buscaEndereco() {   
+    var consultaCEP = await fetch("http://viacep.com.br/ws/01001000/json/") //fetch é um método assincrono com um parâmetro obrigatório, a URL
+    var consultaCEPConvertida = await consultaCEP.json();
+    console.log(consultaCEPConvertida);
+}
+
+buscaEndereco();
