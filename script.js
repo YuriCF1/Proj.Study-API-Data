b66a909d63548f7e1e309ceca2d7241aa688f03b
Yuri <yurifdev@gmail.com>
2022-09-14 21:40:27 -0300

    Getting the post code value to get the adress

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FAZENDO O FETCH COM VÁRIOS 'THEN', CONHECIDO COMO THEN HELL
+
 // var consultaCEP = fetch("http://viacep.com.br/ws/01001000/json/") //fetch é um método assincrono com um parâmetro obrigatório, a URL
 //   .then((resposta) => resposta.json())
 //   .then((r) => {
@@ -12,10 +14,43 @@
 //   .finally((mensagem) => console.log("Processamento concluído")); //Independentemente da resposta, o "finally" irá acontecer.
 // console.log(consultaCEP);
 
-async function buscaEndereco() {   
-    var consultaCEP = await fetch("http://viacep.com.br/ws/01001000/json/") //fetch é um método assincrono com um parâmetro obrigatório, a URL
+// USANDO FUNÇÕES ASYNC AWAIT, COM O USO DE TRY DE CATCH
+
+async function buscaEndereco(cep) {
+  try {
+    // var consultaCEP = await fetch("http://viacep.com.br/ws/01001000/json/"); //fetch é um método assincrono com um parâmetro obrigatório, a URL
+    var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`); //fetch é um método assincrono com um parâmetro obrigatório, a URL
     var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente");
+    }
     console.log(consultaCEPConvertida);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
-buscaEndereco();
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+// buscaEndereco();
+
+// USANDO PROMISSE ALL
+// async function buscaEndereco(cep) {
+//   try {
+//     var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`); //fetch é um método assincrono com um parâmetro obrigatório, a URL
+//     var consultaCEPConvertida = await consultaCEP.json();
+//     if (consultaCEPConvertida.erro) {
+//         throw Error ("CEP não existente");
+//     }
+//     console.log(consultaCEPConvertida);
+//     return consultaCEPConvertida;
+//   } catch (erro) {
+//     console.log(erro);
+//   }
+// }
+
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+// console.log(conjuntoCeps);
+// Promise.all(conjuntoCeps).then(resposta => console.log(resposta))
